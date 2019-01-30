312f719ee27bd5c2b29c885688b0d2f735103be9
jafaircl <jafaircl@me.com>
2019-01-30 13:06:49 -0500

    Update gatsby-ssr.js

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -35,8 +35,10 @@ export const onPreRenderHTML = (
   const isAmp = pathname && pathname.indexOf(pathIdentifier) > -1;
   if (isAmp) {
     const styles = headComponents.reduce((str, x) => {
-      if (x.type === "style" || x.type === "TypographyStyle") {
+      if (x.type === "style") {
         str += x.props.dangerouslySetInnerHTML.__html;
+      } else if (x.key && x.key === "TypographyStyle") {
+        str = `${x.props.typography.toString()}${str}`;
       }
       return str;
     }, "");
@@ -70,7 +72,7 @@ export const onPreRenderHTML = (
       ) : (
         <Fragment />
       ),
-      ...headComponents.filter(x => x.type !== "style" && x.type !== "script")
+      ...headComponents.filter(x => x.type !== "style" && x.type !== "script" && x.key !== "TypographyStyle")
     ]);
     replacePreBodyComponents([
       ...preBodyComponents.filter(x => x.key !== "plugin-google-tagmanager")
